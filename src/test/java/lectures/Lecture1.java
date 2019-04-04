3bc7f4190a47484043b4d6c6fe7d3b95a0871c16
asamonenko <asamonenko@talend.com>
2019-04-04 18:01:27 +0300

    First lecture

--- src/test/java/lectures/Lecture1.java
+++ src/test/java/lectures/Lecture1.java
@@ -6,6 +6,7 @@ import beans.Person;
 
 import com.google.common.collect.ImmutableList;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,15 +19,36 @@ public class Lecture1 {
 
   @Test
   public void imperativeApproach() throws IOException {
-    List<Person> people = MockData.getPeople();
+    List<Person> peoples = MockData.getPeople();
+    List<Person> children = Lists.newArrayList();
+
     // 1. Find people aged less or equal 18
     // 2. Then change implementation to find first 10 people
+    final int limit = 10;
+    int counter = 0;
 
+    for (Person person : peoples) {
+      if (person.getAge() <= 18) {
+        children.add(person);
+        counter++;
+        if (counter == limit)
+          break;
+      }
+    }
+
+    for (Person person : children)
+      System.out.println(person.getAge());
   }
 
   @Test
   public void declarativeApproachUsingStreams() throws Exception {
-    ImmutableList<Person> people = MockData.getPeople();
+    ImmutableList<Person> peoples = MockData.getPeople();
+    final int limit = 10;
 
+    peoples.stream()
+            .filter(p -> p.getAge() <= 18)
+            .limit(limit)
+            .collect(Collectors.toList())
+            .forEach(System.out::println);
   }
 }
