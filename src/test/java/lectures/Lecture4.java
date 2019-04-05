c1896aed097efd91e6dbe0f79fe19095f0005dce
Anna Samonenko <annsamonenko97@gmail.com>
2019-04-05 09:47:44 +0300

    Lecture 5.1

--- src/test/java/lectures/Lecture4.java
+++ src/test/java/lectures/Lecture4.java
@@ -22,7 +22,6 @@ public class Lecture4 {
     @Test
     public void distinctWithSet() throws Exception {
         final List<Integer> numbers = ImmutableList.of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9);
-
         numbers.stream().collect(Collectors.toSet()).forEach(System.out::println);
     }
 }
--- src/test/java/lectures/Lecture5.java
+++ src/test/java/lectures/Lecture5.java
@@ -1,43 +1,49 @@
 package lectures;
 
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 import beans.Car;
 import beans.Person;
 import beans.PersonDTO;
 import com.google.common.collect.ImmutableList;
+
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+
 import mockdata.MockData;
 import org.junit.Test;
 
 public class Lecture5 {
 
-  @Test
-  public void understandingFilter() throws Exception {
-    ImmutableList<Car> cars = MockData.getCars();
+    @Test
+    public void understandingFilter() throws Exception {
+        ImmutableList<Car> cars = MockData.getCars();
+        Predicate<Car> predicate = car -> car.getPrice() < 10000
 
-  }
+        List cheapCars = cars.stream()
+                .filter(predicate)
+                .collect(Collectors.toList());
+        cars.forEach(System.out::println);
+    }
 
-  @Test
-  public void ourFirstMapping() throws Exception {
-    // transform from one data type to another
-    List<Person> people = MockData.getPeople();
+    @Test
+    public void ourFirstMapping() throws Exception {
+        // transform from one data type to another
+        List<Person> people = MockData.getPeople();
 
-  }
+    }
 
-  @Test
-  public void averageCarPrice() throws Exception {
-    // calculate average of car prices
+    @Test
+    public void averageCarPrice() throws Exception {
+        // calculate average of car prices
 
-  }
+    }
 
-  @Test
-  public void test() throws Exception {
+    @Test
+    public void test() throws Exception {
 
-  }
+    }
 }
 
 
